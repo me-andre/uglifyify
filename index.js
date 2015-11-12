9f500f2e7bb8992ef26f72bef8dfd031a0d75396
meandre <andrew.pazniak@gmail.com>
2015-11-12 14:50:02 +0300

    semicolons which i missed so much

--- index.js
+++ index.js
@@ -1,119 +1,119 @@
 var minimatch = require('minimatch').Minimatch
-  , convert = require('convert-source-map')
-  , through = require('through')
-  , path = require('path')
-  , ujs = require('uglify-js')
-  , extend = require('extend')
+    , convert = require('convert-source-map')
+    , through = require('through')
+    , path = require('path')
+    , ujs = require('uglify-js')
+    , extend = require('extend');
 
-module.exports = uglifyify
+module.exports = uglifyify;
 
 function uglifyify(file, opts) {
-  opts = opts || {}
+    opts = opts || {};
 
-  var debug = '_flags' in opts
-    ? opts._flags.debug
-    : true
+    var debug = '_flags' in opts
+        ? opts._flags.debug
+        : true;
 
-  delete opts._flags
+    delete opts._flags;
 
-  if (ignore(file, opts.ignore)) {
-    return through()
-  }
-
-  var buffer = ''
-  var exts = []
-    .concat(opts.exts || [])
-    .concat(opts.x || [])
-    .map(function(d) {
-      if (d.charAt(0) === '.') return d
-      return '.' + d
-    })
-
-  if (
-    /\.json$/.test(file) ||
-    exts.length &&
-    exts.indexOf(path.extname(file)) === -1
-  ) {
-    return through()
-  }
-
-  return through(function write(chunk) {
-    buffer += chunk
-  }, capture(function ready() {
-    var matched = buffer.match(
-      /\/\/[#@] ?sourceMappingURL=data:application\/json;base64,([a-zA-Z0-9+\/]+)={0,2}\n?$/
-    )
-
-    debug = opts.sourcemap !== false && (debug || matched)
-    opts  = extend({}, {
-      fromString: true
-      , compress: true
-      , mangle: true
-      , filename: file
-      , sourceMaps: debug
-    }, opts)
-
-    if (typeof opts.compress === 'object') {
-      delete opts.compress._
+    if (ignore(file, opts.ignore)) {
+        return through();
     }
 
-    if (debug) opts.outSourceMap = 'out.js.map'
+    var buffer = '';
+    var exts = []
+        .concat(opts.exts || [])
+        .concat(opts.x || [])
+        .map(function (d) {
+            if (d.charAt(0) === '.') return d;
+            return '.' + d
+        });
 
-    // Check if incoming source code already has source map comment.
-    // If so, send it in to ujs.minify as the inSourceMap parameter
-    if (debug && matched) {
-      opts.inSourceMap = convert.fromJSON(
-        new Buffer(matched[1], 'base64').toString()
-      ).sourcemap
+    if (
+        /\.json$/.test(file) ||
+        exts.length &&
+        exts.indexOf(path.extname(file)) === -1
+    ) {
+        return through()
     }
 
-    buffer = 'function module(){\n' + buffer + '\n}';
+    return through(function write(chunk) {
+        buffer += chunk
+    }, capture(function ready() {
+        var matched = buffer.match(
+            /\/\/[#@] ?sourceMappingURL=data:application\/json;base64,([a-zA-Z0-9+\/]+)={0,2}\n?$/
+        );
 
-    var min = ujs.minify(buffer, opts);
+        debug = opts.sourcemap !== false && (debug || matched);
+        opts = extend({}, {
+            fromString: true
+            , compress: true
+            , mangle: true
+            , filename: file
+            , sourceMaps: debug
+        }, opts);
 
-    // Wrapping the code in the function allows uglify to drop unused named functions
-    // that are declared in the module top-level scope
-    min.code = function(code) {
-      var start = code.indexOf('{');
-      var end = code.lastIndexOf('}');
-      return code.slice(start + 1, end);
-    }(min.code);
-    this.queue(min.code)
+        if (typeof opts.compress === 'object') {
+            delete opts.compress._
+        }
 
-    if (min.map && min.map !== 'null') {
-      var map = convert.fromJSON(min.map)
+        if (debug) opts.outSourceMap = 'out.js.map';
 
-      map.setProperty('sources', [file])
-      map.setProperty('sourcesContent', matched
-        ? opts.inSourceMap.sourcesContent
-        : [buffer]
-      )
+        // Check if incoming source code already has source map comment.
+        // If so, send it in to ujs.minify as the inSourceMap parameter
+        if (debug && matched) {
+            opts.inSourceMap = convert.fromJSON(
+                new Buffer(matched[1], 'base64').toString()
+            ).sourcemap
+        }
 
-      this.queue('\n')
-      this.queue(map.toComment())
+        buffer = 'function module(){\n' + buffer + '\n}';
+
+        var min = ujs.minify(buffer, opts);
+
+        // Wrapping the code in the function allows uglify to drop unused named functions
+        // that are declared in the module top-level scope
+        min.code = function (code) {
+            var start = code.indexOf('{');
+            var end = code.lastIndexOf('}');
+            return code.slice(start + 1, end);
+        }(min.code);
+        this.queue(min.code);
+
+        if (min.map && min.map !== 'null') {
+            var map = convert.fromJSON(min.map);
+
+            map.setProperty('sources', [file]);
+            map.setProperty('sourcesContent', matched
+                    ? opts.inSourceMap.sourcesContent
+                    : [buffer]
+            );
+
+            this.queue('\n');
+            this.queue(map.toComment())
+        }
+
+        this.queue(null)
+    }));
+
+    function capture(fn) {
+        return function () {
+            try {
+                fn.apply(this, arguments)
+            } catch (err) {
+                return this.emit('error', err)
+            }
+        }
     }
-
-    this.queue(null)
-  }))
-
-  function capture(fn) {
-    return function() {
-      try {
-        fn.apply(this, arguments)
-      } catch(err) {
-        return this.emit('error', err)
-      }
-    }
-  }
 }
 
 function ignore(file, list) {
-  if (!list) return
+    if (!list) return;
 
-  list = Array.isArray(list) ? list : [list]
+    list = Array.isArray(list) ? list : [list];
 
-  return list.some(function(pattern) {
-    var match = minimatch(pattern)
-    return match.match(file)
-  })
+    return list.some(function (pattern) {
+        var match = minimatch(pattern);
+        return match.match(file)
+    })
 }
